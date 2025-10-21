726af54f7c3ac5475956a13dda0bafc7e580233e
sksksk54-gif <sksksk54@naver.com>
2025-10-21 17:40:57 +0900

    스크립트를 바꾸었어용

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
