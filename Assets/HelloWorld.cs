f6f3848b605b777254445eb6a2519a7796833ba8
sksksk54-gif <sksksk54@naver.com>
2025-10-22 13:13:47 +0900

    Update HelloWorld.cs

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("한글이다");
+        Debug.Log("성광이 병합을 냄");
+<<<<<<< HEAD
+        Debug.Log("한글한글한글 수정수정수정");
+=======
+        Debug.Log("성광이 병합을 또 냄");
+>>>>>>> af135c6bea54fb9ed14bb50875a8ffac4f418264
     }
 
     // Update is called once per frame
