67fbec9fa7838e4063763406b7be08616dad30fd
goingtodkworld-cpu <goingtodkworld@gmail.com>
2025-10-22 11:57:29 +0900

    Change log message from 'Hello world' to 'Ha'_251022단경

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world");
+        Debug.Log("Ha");
     }
 
     // Update is called once per frame
