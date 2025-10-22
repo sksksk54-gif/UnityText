41176ca0d878db618641f6c95d13d7f636f7a36c
goingtodkworld-cpu <goingtodkworld@gmail.com>
2025-10-22 15:58:20 +0900

    케케케

---
+++ Assets/kkkkkkk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kkkkkkk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("追追追追");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
