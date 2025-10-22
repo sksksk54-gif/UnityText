e6c8dacb81db34e7cb8baa9ce127e668ef9e675d
sksksk54-gif <sksksk54@naver.com>
2025-10-22 13:17:47 +0900

    Update HelloWorld.cs

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,11 +7,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("성광이 병합을 냄");
+        Debug.Log("성광이 충돌을 냄");
 <<<<<<< HEAD
         Debug.Log("한글한글한글 수정수정수정");
 =======
-        Debug.Log("성광이 병합을 또 냄");
+        Debug.Log("성광이 충돌을 또 냄");
 >>>>>>> af135c6bea54fb9ed14bb50875a8ffac4f418264
     }
 
